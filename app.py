dd031e9acce3eb45cb3e9abbfc9797e7d414fa53
Lucas Pagac <lucaspagac0@gmail.com>
2020-12-02 16:29:36 -0800

    madlib created

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ debug = DebugToolbarExtension(app)
 
 @app.route("/questions")
 def show_questions():
-    """ Shows the form that requires inputs needed to create 
+    """ Shows the form that requires inputs needed to create
     story instance """
 
     form_inputs = story.prompts
@@ -19,4 +19,11 @@ def show_questions():
     return render_template("questions.html", prompts=form_inputs)
 
 
-# @app.route("/story")
+@app.route("/story")
+def make_story():
+    """ Create a story page from user input on /questions """
+
+    user_input = {key: request.args[key] for key in request.args.keys()}
+    text = story.generate(user_input)
+
+    return render_template('story.html', user_story=text)
